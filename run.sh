6cac361d850c5f97f7307c801e7baa2306a1b1c0
yttnn <67893139+yttnn@users.noreply.github.com>
2023-12-05 22:27:37 +0900

    finish boot(section 5)

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ set -xue
 QEMU=$HOME/tools/qemu-riscv32-8.1.2/bin/qemu-system-riscv32
 # path of clang
 CLANG=clang
-CFLAGS="-c -std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib"
+CFLAGS="-c -std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib -mno-relax"
 # linker
 LINKER=ld.lld
 LDFLAGS="-m elf32lriscv -L/lib -Tkernel.ld -Map=kernel.map"
@@ -15,5 +15,5 @@ $CLANG $CFLAGS -o kernel.o kernel.c
 $LINKER $LDFLAGS kernel.o -o kernel.elf
 
 # launch qemu
-# $QEMU -machine virt -bios opensbi-riscv32-generic-fw_dynamic.bin -nographic -serial mon:stdio --no-reboot \
-#     -kernel kernel.elf
+$QEMU -machine virt -bios opensbi-riscv32-generic-fw_dynamic.bin -nographic -serial mon:stdio --no-reboot \
+    -kernel kernel.elf
