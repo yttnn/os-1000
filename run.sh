d4602fdd3ef6cc626bb28051c74d97e163cf8226
yttnn <67893139+yttnn@users.noreply.github.com>
2023-12-05 10:33:47 +0900

    [wip] boot is not running

--- run.sh
+++ run.sh
@@ -4,7 +4,14 @@ set -xue
 
 # file path of qemu
 QEMU=$HOME/tools/qemu-riscv32-8.1.2/bin/qemu-system-riscv32
+# path of clang
+CLANG=clang
+CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib -v"
+
+# build kernel
+$CLANG $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
+    kernel.c
 
 # launch qemu
-$QEMU -machine virt -bios opensbi-riscv32-generic-fw_dynamic.bin -nographic -serial mon:stdio --no-reboot
-# $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot
+# $QEMU -machine virt -bios opensbi-riscv32-generic-fw_dynamic.bin -nographic -serial mon:stdio --no-reboot \
+#     -kernel kernel.elf
