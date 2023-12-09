0b02071e89db58ceab23223bf0dac137f29bf698
yttnn <67893139+yttnn@users.noreply.github.com>
2023-12-09 22:18:30 +0900

    8 kernel panic

--- kernel.c
+++ kernel.c
@@ -33,6 +33,9 @@ void putchar(char ch) {
 void kernel_main(void) {
   memset(__bss, 0, (size_t) __bss_end - (size_t) __bss);
 
+  PANIC("booted!");
+  printf("unreachable here!\n");
+
   // const char *s = "\n\nHello World!\n";
   // for (int i = 0; s[i] != '\0'; i++) {
   //   putchar(s[i]);
