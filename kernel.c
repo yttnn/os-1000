0283f2e7581bcdee2e608c593d64f08fc71493de
yttnn <67893139+yttnn@users.noreply.github.com>
2023-12-09 22:00:38 +0900

    6 stdlib

--- kernel.c
+++ kernel.c
@@ -29,14 +29,6 @@ void putchar(char ch) {
   sbi_call(ch, 0, 0, 0, 0, 0, 0, 1); // console putchar (eid=0x01)
 }
 
-void *memset(void *buf, char c, size_t n) {
-  uint8_t *p = (uint8_t *) buf;
-  while (n--) {
-    *p++ = c;
-  }
-
-  return buf;
-}
 
 void kernel_main(void) {
   memset(__bss, 0, (size_t) __bss_end - (size_t) __bss);
