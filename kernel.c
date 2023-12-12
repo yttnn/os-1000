eacf373174f921083ad82e47c6d20e11abcf4678
yttnn <67893139+yttnn@users.noreply.github.com>
2023-12-12 21:21:50 +0900

    remove comment

--- kernel.c
+++ kernel.c
@@ -261,19 +261,6 @@ void kernel_main(void) {
   
   yield();
   PANIC("switched to idle process");
-  // paddr_t paddr0 = alloc_pages(2);
-  // paddr_t paddr1 = alloc_pages(1);
-  // printf("alloc_pages test: paddr0=%x\n", paddr0);
-  // printf("alloc_pages test: paddr1=%x\n", paddr1);
-  // PANIC("booted");
-  // WRITE_CSR(stvec, (uint32_t) kernel_entry);
-  // __asm__ __volatile__("unimp");
-
-  // PANIC("booted!");
-  // printf("unreachable here!\n");
-
-  // printf("\n\nHello %s\n", "World!");
-  // printf("1 + 2 = %d, %x\n", 1 + 2, 0x1234abcd);
 
   for (;;);
 }
