a9e61e56bb78e7493c6eadac498750151ba934cb
yttnn <67893139+yttnn@users.noreply.github.com>
2023-12-18 17:54:51 +0900

    15 getchar

--- kernel.c
+++ kernel.c
@@ -39,10 +39,25 @@ void putchar(char ch) {
   sbi_call(ch, 0, 0, 0, 0, 0, 0, 1); // console putchar (eid=0x01)
 }
 
+long getchar(void) {
+  struct sbiret ret = sbi_call(0, 0, 0, 0, 0, 0, 0, 2);
+  return ret.error;
+}
+
 void handle_syscall(struct trap_frame *f) {
   switch (f->a3) {
     case SYS_PUTCHAR:
       putchar(f->a0);
+      break;
+    case SYS_GETCHAR:
+      while (1) {
+        long ch = getchar();
+        if (ch >= 0) {
+          f->a0 = ch;
+          break;
+        }
+        yield();
+      }
       break;
     default:
       PANIC("unexpected syscall a3=%x\n", f->a3);
--- user.c
+++ user.c
@@ -26,6 +26,10 @@ void putchar(char ch) {
   syscall(SYS_PUTCHAR, ch, 0, 0);
 }
 
+int getchar(void) {
+  return syscall(SYS_GETCHAR, 0, 0, 0);
+}
+
 __attribute__((section(".text.start")))
 __attribute__((naked))
 void start(void) {
