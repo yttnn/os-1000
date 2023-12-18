9ee681c9572941ad43b9c6ada587ea557a8343da
yttnn <67893139+yttnn@users.noreply.github.com>
2023-12-18 17:10:33 +0900

    14

--- kernel.c
+++ kernel.c
@@ -2,6 +2,7 @@
 #include "common.h"
 
 void map_page(uint32_t *table1, uint32_t vaddr, paddr_t paddr, uint32_t flags);
+void user_entry(void);
 
 typedef unsigned char uint8_t;
 typedef unsigned int uint32_t;
@@ -10,6 +11,7 @@ typedef uint32_t size_t;
 extern char __bss[], __bss_end[], __stack_top[];
 extern char __free_ram[], __free_ram_end[];
 extern char __kernel_base[];
+extern char _binary_shell_bin_start[], _binary_shell_bin_size[];
 
 struct process procs[PROCS_MAX];
 struct process *current_proc;
@@ -94,7 +96,7 @@ __attribute__((naked)) void switch_context(uint32_t *prev_sp, uint32_t *next_sp)
   );
 }
 
-struct process *create_process(uint32_t pc) {
+struct process *create_process(const void *image, size_t image_size) {
   struct process *proc = NULL;
   int i;
   for (i = 0; i < PROCS_MAX; i++) {
@@ -120,7 +122,7 @@ struct process *create_process(uint32_t pc) {
   *--sp = 0;             // s2
   *--sp = 0;             // s1
   *--sp = 0;             // s0
-  *--sp = (uint32_t) pc; // ra
+  *--sp = (uint32_t) user_entry; // ra
 
   uint32_t *page_table = (uint32_t *) alloc_pages(1);
 
@@ -128,6 +130,12 @@ struct process *create_process(uint32_t pc) {
        paddr < (paddr_t) __free_ram_end;
        paddr += PAGE_SIZE) {
     map_page(page_table, paddr, paddr, PAGE_R | PAGE_W | PAGE_X);
+  }
+
+  for (uint32_t off = 0; off < image_size; off += PAGE_SIZE) {
+    paddr_t page = alloc_pages(1);
+    memcpy((void *) page, image + off, PAGE_SIZE);
+    map_page(page_table, USER_BASE + off, page, PAGE_U | PAGE_R | PAGE_W | PAGE_X);
   }
 
   proc->pid = i + 1;
@@ -182,6 +190,17 @@ void map_page(uint32_t *table1, uint32_t vaddr, paddr_t paddr, uint32_t flags) {
   uint32_t vpn0 = (vaddr >> 12) & 0x3ff;
   uint32_t *table0 = (uint32_t *) ((table1[vpn1] >> 10) * PAGE_SIZE);
   table0[vpn0] = ((paddr / PAGE_SIZE) << 10) | flags | PAGE_V;
+}
+
+__attribute__((naked)) void user_entry(void) {
+  __asm__ __volatile__(
+    "csrw sepc, %[sepc]\n"
+    "csrw sstatus, %[sstatus]\n"
+    "sret\n"
+    :
+    : [sepc] "r" (USER_BASE),
+      [sstatus] "r" (SSTATUS_SPIE)
+  );
 }
 
 __attribute__((naked))
@@ -299,12 +318,13 @@ void kernel_main(void) {
 
   WRITE_CSR(stvec, (uint32_t) kernel_entry);
 
-  idle_proc = create_process((uint32_t) NULL);
+  idle_proc = create_process(NULL, 0);
   idle_proc->pid = -1; // idle process
   current_proc = idle_proc;
 
-  proc_a = create_process((uint32_t) proc_a_entry);
-  proc_b = create_process((uint32_t) proc_b_entry);
+  // proc_a = create_process((uint32_t) proc_a_entry);
+  // proc_b = create_process((uint32_t) proc_b_entry);
+  create_process(_binary_shell_bin_start, (size_t) _binary_shell_bin_size);
   
   yield();
   PANIC("switched to idle process");
